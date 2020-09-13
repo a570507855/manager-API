d963219532cfb714a9f03168eef55c36137c30a5
admin <570507855@qq.com>
2020-09-13 11:26:46 +0800

    fix:#添加了cors配置

--- src/infrastructure/component/express-server.ts
+++ src/infrastructure/component/express-server.ts
@@ -13,19 +13,24 @@ export class ExpressServer extends ComponentBase {
     useContainer(Container);
 
     const app = express();
+    const corsOptions = {
+      'orgin': '*',
+      'methods': 'POST,GET,OPTIONS',
+      'allowedHeaders': 'Content-Type',
+      'maxAge': '2592000'
+    }
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(cors(corsOptions));
 
-    app.use(cors());
-
-    app.all('*', (req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', '*');
-      if (req.method === 'OPTIONS') {
-        res.setHeader('Access-Control-Max-Age', '2592000'); //将options缓存起来，避免发送多次options
-      }
-      next();
-    });
+    // app.all('*', (req, res, next) => {
+    //   // res.setHeader('Access-Control-Allow-Origin', '*');
+    //   // res.setHeader('Access-Control-Allow-Headers', '*');
+    //   // if (req.method === 'OPTIONS') {
+    //   //   res.setHeader('Access-Control-Max-Age', '2592000'); //将options缓存起来，避免发送多次options
+    //   // }
+    //   next();
+    // });
     useExpressServer(app, {
       controllers: this.m_controllers,
     }).listen(8888);
